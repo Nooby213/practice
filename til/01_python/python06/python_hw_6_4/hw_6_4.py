7ee864fd41f4fe0a9ac88d32789dcfa8a4175c60
이승민 <lsm5631@naver.com>
2024-01-23 12:43:42 +0900

    6일차 실습과제

---
+++ til/01_python/python06/python_hw_6_4/hw_6_4.py
@@ -0,0 +1,10 @@
+# 아래 함수를 수정하시오.
+def add_item_to_dict(data, key, value):
+    new_dict = data.copy()
+    add_dict = {key : value}
+    new_dict.update(add_dict)
+    return new_dict
+
+my_dict = {'name': 'Alice', 'age': 25}
+result = add_item_to_dict(my_dict, 'country', 'USA')
+print(result)
