600651f66036ef91b7f8fff14773af7443d8aa4e
Nooby213 <lsm5631@naver.com>
2024-01-24 23:07:36 +0900

    7일차 실습과제

--- til/01_python/python07/python_ws_7_2/ws_7_2.py
+++ til/01_python/python07/python_ws_7_2/ws_7_2.py
@@ -1,8 +1,10 @@
 # 아래 클래스를 수정하시오.
 class Shape:
+    
     def __init__(self, width, height):
         self.width = width
         self.height = height
+
     def calculate_area(self):
         return self.width * self.height
 
