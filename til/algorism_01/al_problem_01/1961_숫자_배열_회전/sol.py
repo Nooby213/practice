b03218e2b8cd1c9825b00ee68797f63d2641f16d
Nooby213 <lsm5631@naver.com>
2024-02-02 00:00:04 +0900

    4일차 알고리즘 수업 집에서 복습

---
+++ til/algorism_01/al_problem_01/1961_숫자_배열_회전/sol.py
@@ -0,0 +1,36 @@
+import sys
+
+sys.stdin = open('input.txt')
+
+T = int(input())
+
+
+def rotation(alist):  # 회전 함수
+    alist_copy = [[0] * N for _ in range(N)]
+    for n in range(N):
+        for m in range(N):
+            alist_copy[n][m] = alist[2 - m][n]
+    return alist_copy
+
+
+for t in range(1, T + 1):  # 테스트 케이스 반복
+    N = int(input())  # N * N 행렬
+
+    alist = [list(map(int, input().split())) for _ in range(N)]
+    # print(alist)
+
+    rotation_90 = rotation(alist)
+    rotation_180 = rotation(rotation_90)
+    rotation_270 = rotation(rotation_180)
+
+    # print(rotation_90)
+    # print(rotation_180)
+    # print(rotation_270)
+
+    row1 = [*rotation_90[0], *rotation_180[0], *rotation_270[0]]
+    row2 = [*rotation_90[1], *rotation_180[1], *rotation_270[1]]
+    row3 = [*rotation_90[2], *rotation_180[2], *rotation_270[2]]
+
+    for n in range(N):
+        for m in range(N):
+            print()
