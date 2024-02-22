397fbf362f92052c7c36492b03d9b3ef6aa90898
Nooby213 <lsm5631@naver.com>
2024-02-23 01:02:53 +0900

    240223 백준문제풀이

--- til/백준 문제 풀이/silver/2304.py
+++ til/백준 문제 풀이/silver/2304.py
@@ -5,7 +5,7 @@
 N = int(input())  # N 개 막대 기둥 1 <= N <= 1000
 col_lst = [list(map(int, input().split())) for _ in range(N)]
 col_sorted = sorted(col_lst, key=lambda x: x[0])  # 순서 별로 정렬
-area = 0  # 넓이
+
 idx_highest_col = 0  # 최고 높은 기둥 인덱스
 highest_col = 0  # 최고 높은 기둥 높이
 
@@ -14,12 +14,12 @@ for i in range(N):
         highest_col = col_sorted[i][1]
         idx_highest_col = i
 
-# 최고층 기둥의 위치까지
+
+area = highest_col
+
+# 최고층 기둥의 위치 전까지
 mx_col = 0
-for i in range(N - idx_highest_col):
-    if col_sorted[i][1] == highest_col:
-        area += highest_col
-        continue
+for i in range(idx_highest_col):
     if col_sorted[i][1] > mx_col:
         mx_col = col_sorted[i][1]
     area += mx_col * (col_sorted[i + 1][0] - col_sorted[i][0])
