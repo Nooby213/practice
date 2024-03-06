91f74d8324133f5f6ae0492fd0002091e4ba53e5
Nooby213 <lsm5631@naver.com>
2024-03-06 22:33:09 +0900

    240306 백준문제풀이

--- til/백준 문제 풀이/gold/1107_리모컨.py
+++ til/백준 문제 풀이/gold/1107_리모컨.py
@@ -1,40 +1,30 @@
-# +, -, 0에서는 채널 안 바뀜
-# 버튼 최소 몇 번 눌러야됨
+# 가능한 채널인지 확인
+def possible(channel):
+    for c in str(channel):
+        if int(c) in broken:
+            return False
+    else:
+        return True
+
+
 # 현재 채널 100 > 채널 N으로 이동, 0 ≤ N ≤ 500,000
 N = input()
-# 고장난 버튼 M개, 0 ≤ M ≤ 10
+# # 고장난 버튼 M개, 0 ≤ M ≤ 10
 M = int(input())
-# 고장난 버튼
-broken = list(map(str, input().split()))
-goal = list(i for i in N)
-now = 100
-temp_channel = ''
-cnt = 0
-updn_cnt = abs(int(N) - now)
-
-for i in N:
-    cnt += 1
-    if i not in broken:
-        temp_channel += i
-    else:
-        j = int(i) - 1
-        k = int(i) + 1
-        if 0 <= j < 10 and str(j) not in broken:
-            temp_channel += str(j)
-            continue
-        elif 0 <= k < 10 and str(k) not in broken:
-            temp_channel += str(k)
-            continue
-        while (0 <= j < 10 and str(j) in broken) or (0 <= k < 10 and str(k) in broken):
-            j -= 1
-            k += 1
-            if 0 <= j < 10 and str(j) not in broken:
-                temp_channel += str(j)
-                break
-            elif 0 <= k < 10 and str(k) not in broken:
-                temp_channel += str(k)
-                break
+if M:
+    broken = list(map(int, input().split()))
 else:
-    cnt += abs(int(temp_channel) - int(N))
+    broken = []
 
-print(min(updn_cnt, cnt))
+min_press = abs(int(N) - 100)
+
+# 모든 채널 확인
+for channel in range(1000001):
+    if broken and possible(channel):
+        min_press = min(min_press, abs(channel - int(N)) + len(str(channel)))
+    if not broken:
+        min_press = min(min_press, abs(channel - int(N)) + len(str(channel)))
+    if min_press == 0:
+        break
+
+print(min_press)
