5ec4dc769ceae53a99bd1b6811c91f0bebd11b85
Nooby213 <lsm5631@naver.com>
2024-03-21 21:30:17 +0900

    240321 알고리즘 수업 그래프

--- til/algorism_02/240321/5251_최소이동거리/sol.py
+++ til/algorism_02/240321/5251_최소이동거리/sol.py
@@ -4,29 +4,37 @@ sys.stdin = open('input.txt')
 from heapq import heappop, heappush
 
 
-def bfs():
-    q = [(0, 0)]
-    min_dis = 11 * E
-    sum_weight = 0
+def dijkstra(start):
+    q = []
+    heappush(q, (0, start))
+    distance[start] = 0
     while q:
         weight, now = heappop(q)
 
-        sum_weight += weight
-        if now == N:
-            min_dis = min(min_dis, sum_weight)
+        if distance[now] < weight:
+            continue
 
         for w, to in adj[now]:
-            heappush(q, (w, to))
+            new_weight = w + weight
 
-    return min_dis
+            if new_weight >= distance[to]:
+                continue
+
+            distance[to] = new_weight
+            heappush(q, (new_weight, to))
+
+
+
 
 T = int(input())
 for t in range(1, T + 1):
     N, E = map(int, input().split())
     adj = [[] for _ in range(N + 1)]
-
+    inf = float('inf')
+    distance = [inf] * (N + 1)
     for i in range(E):
         s, e, w = map(int, input().split())
         adj[s].append((w, e))
 
-    print(f'#{t} {bfs()}')
+    dijkstra(0)
+    print(f'#{t} {distance[N]}')
